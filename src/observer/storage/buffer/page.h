91c033f3e687f9b8dcdea330b1b1268021411349
Wenbin <85331908+Wenbin1002@users.noreply.github.com>
2024-03-26 14:22:16 +0800

    add crc checksum to disk buffer page #356 (#357)

--- src/observer/storage/buffer/page.h
+++ src/observer/storage/buffer/page.h
@@ -24,7 +24,7 @@ static constexpr int BP_INVALID_PAGE_NUM = -1;
 static constexpr PageNum BP_HEADER_PAGE = 0;
 
 static constexpr const int BP_PAGE_SIZE      = (1 << 13);
-static constexpr const int BP_PAGE_DATA_SIZE = (BP_PAGE_SIZE - sizeof(PageNum) - sizeof(LSN));
+static constexpr const int BP_PAGE_DATA_SIZE = (BP_PAGE_SIZE - sizeof(PageNum) - sizeof(LSN) - sizeof(CheckSum));
 
 // TEXT page 里面一个slot大小为64字节
 static constexpr const int BP_TEXT_SLOT_SIZE = 64;
@@ -35,7 +35,8 @@ static constexpr const int BP_TEXT_SLOT_SIZE = 64;
  */
 struct Page
 {
-  PageNum page_num;
-  LSN     lsn;
-  char    data[BP_PAGE_DATA_SIZE];
+  PageNum  page_num;
+  LSN      lsn;
+  CheckSum check_sum;
+  char     data[BP_PAGE_DATA_SIZE];
 };
